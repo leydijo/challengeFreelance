a294168486a19c0430974a5111a5e64f2345a10f
leydijo <leydijohana182010@hotmail.com>
2019-02-17 17:31:53 -0300

    finished

--- index.js
+++ index.js
@@ -4,29 +4,29 @@
 // obtenerPrimos([6,4,3,7,3,8,11])
 // salida: [11,7,3,3]
 
-// let primos = [6,4,3,7,3,8,11]
+let primos = [6,4,3,7,3,8,11]
 
 
-// let lista = [7, 2, 5, 20, 11, 9]; 
-//  let temp = 0; 
-//  let Mayor = 0; 
-//  let num = []; 
-//  lista.sort (function(a, b) {return b - a}); 
-//  for (let j = 0; j < lista.length; j ++) { 
-//  for (let i = 2; i <lista [j]; i ++) { 
-//  if(lista [j]% i == 0) { 
-//  temp = temp + 1; 
-//  } 
-//  } 
-//  if(temp> 0) { 
+let lista = [7, 2, 5, 20, 11, 9]; 
+ let temp = 0; 
+ let Mayor = 0; 
+ let num = []; 
+ lista.sort (function(a, b) {return b - a}); 
+ for (let j = 0; j < lista.length; j ++) { 
+ for (let i = 2; i <lista [j]; i ++) { 
+ if(lista [j]% i == 0) { 
+ temp = temp + 1; 
+ } 
+ } 
+ if(temp> 0) { 
  
-//  } else { 
-//     num[j]=lista[j];
+ } else { 
+    num[j]=lista[j];
 
-//  } 
-//  temp = 0; 
-//  } 
-//  alert  ('Los numeros ' + num + ' son primos'); 
+  } 
+ temp = 0; 
+ } 
+ alert  ('Los numeros ' + num + ' son primos'); 
 
 
  //segundo punto
@@ -44,16 +44,16 @@
 
 
 
-let  vacio = [];
+// let  vacio = [];
 
-function ordenar(){
+// function ordenar(){
     
-    let letras = document.getElementById("letras").value ;
-    let mostrar = document.getElementById("mostrar");
-    vacio = letras.split("").sort().reverse().join("");
-    mostrar.innerHTML = vacio;
+//     let letras = document.getElementById("letras").value ;
+//     let mostrar = document.getElementById("mostrar");
+//     vacio = letras.split("").sort().reverse().join("");
+//     mostrar.innerHTML = vacio;
      
     
-}
+// }
 
 
